93b392e0cca923595768c2156480ce06c73a4a37
Sudhir Yadav <18092857+sudhirya@users.noreply.github.com>
2023-04-01 17:25:14 +0530

    Request object and introduction to its properties

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const http = require('http')
 
 
 const server = http.createServer((req, res)=>{
-    console.log(req);
-    process.exit();
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
 });
 
 server.listen(3002);
