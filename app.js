76a5bcb1789c8ec67d2b8a83e96c67cf87cbb1d2
Sudhir Yadav <sudhiryadav347@gmail.com>
2023-05-18 01:23:22 +0530

    #16 Adding middleware, how to send response. How request travels through middlewares.

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express.Js!</h1>');
+});
+
 const server = http.createServer(app);
 server.listen(3002);
