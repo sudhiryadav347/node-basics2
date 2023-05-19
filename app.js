8f3298d1d42bdd656a7768e359b65fb8ef33f2f2
Sudhir Yadav <sudhiryadav347@gmail.com>
2023-05-19 14:54:56 +0530

    Adding and returning a 404 Page.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -10,9 +11,10 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-//Handle 404 
+//Handle 404
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page Not Found!</h1>");
+	// res.status(404).send("<h1>Page Not Found!</h1>");
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3002);
