ee6ab9cfdf44938e79fb4e8c0e17b1d2d1ae736d
Sudhir Yadav <sudhiryadav347@gmail.com>
2023-05-24 23:51:52 +0530

    Configuring pug template engine for using in application.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const path = require('path');
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
