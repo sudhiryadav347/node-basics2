7aef21016ead9c31557fcbf6f716d85c8aeb858d
Sudhir Yadav <sudhiryadav347@gmail.com>
2023-05-26 00:18:27 +0530

    Outputting dynamic content in put template and converting shop.html to shop.pug

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,8 @@ const adminData = require('../routes/admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log(adminData.products);
-    res.render('shop');
+    const products = adminData.products;
+    res.render('shop', { prods: products, docTitle: 'Shop' });
 });
 
 
